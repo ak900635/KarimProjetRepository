a6e4ca6b1b9ce6806e025ff09b0d2d9a2c0d8963
L3I <arifa.karim>
2014-05-28 13:39:29 -0700

    commit finale

--- spec/requests/personnes_spec.rb
+++ spec/requests/personnes_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe "Personnes" do
  
-   it "should have the content 'Listing  personnes'" do
+   it "should have the content 'Listing personnes'" do
       visit '/personnes'
-      expect(page).to have_content('Listing des personnes')
+      expect(page).to have_content('Listing personnes')
     end
 end
