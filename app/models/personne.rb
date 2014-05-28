998947e7c66cd0e81894ea2707c71768fda4afd6
L3I <arifa.karim>
2014-05-28 06:15:46 -0700

    ajout des relations

--- app/models/personne.rb
+++ app/models/personne.rb
@@ -1,5 +1,5 @@
 class Personne < ActiveRecord::Base
 	belongs_to :scene
 	validates :nom, :length => { :maximum => 175 , :minimum => 1 }
-	validates :scene_id, :length => { :maximum => 4, :minimum => 1 }
+	validates :scene_id, :length => { :maximum => 4}
 end
